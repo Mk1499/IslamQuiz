92e2d1c327a7c2bc2403f0284af768d7017e8c1d
mk1499 <m.khaled1499@gmail.com>
2024-08-21 20:17:34 +0300

    align before del

--- src/Services/api-service.ts
+++ src/Services/api-service.ts
@@ -4,8 +4,9 @@ import {getActiveLang} from '../translate';
 import Storage from './storage-service';
 
 // const baseURL = 'https://iquiz-server.onrender.com';
-const baseURL = 'http://192.168.1.106:9000';
-// const baseURL = 'https://drab-lime-donkey-wrap.cyclic.app';
+// const baseURL = 'http://192.168.1.106:9000';
+const baseURL =
+  'https://us-central1-node-api-884c0.cloudfunctions.net/iquizServer';
 export const get = async (url: string, authReq = true) => {
   let headers;
   if (authReq) {
